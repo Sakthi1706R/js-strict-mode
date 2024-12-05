7794a846d69fadc2acf2a94af2cc100bc72db17e
sakthivel R <bestfriend1706@gmail.com>
2024-12-05 18:44:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict'; // To reduce error so use this world('use strict') in the first line;
+
+let user = 'Sakthi';
+
+// use = 'Arun'; // wrong key world, but you not use ('use strict') this they will answer it;
+
+console.log(user);
+
+// const interface = 'Car'; // reserved key world(interface);
+// const private = 'Jack'; // reserved key world(private);
